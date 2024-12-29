f3a8b706be4fb6fd486d8d86a218930786679a5d
Darwin Perdomo <ZeroBl@hotmail.com>
2024-12-29 19:35:23 -0400

    feat(network/ch12): initial structure

---
+++ network/ch12/cmd/housework.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/ZeroBl21/network/ch12/housework"
+)
+
+var dataFile string
+
+func init() {
+	flag.StringVar(&dataFile, "file", "housework.db", "data file")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			`Usage: %s [flags] [add chore, ...|complete #]
+			add				add comma-separated chores
+			complete	complete designated chore
+
+			Flags:`, filepath.Base(os.Args[0]),
+		)
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	var err error
+
+	switch strings.ToLower(flag.Arg(0)) {
+	case "add":
+		err = add(strings.Join(flag.Args()[1:], " "))
+	case "complete":
+		err = complete(flag.Arg(1))
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := list; err != nil {
+		log.Fatal(err)
+	}
+}
+
+func load() ([]*housework.Chore, error) {
+	if _, err := os.Stat(dataFile); os.IsNotExist(err) {
+		return make([]*housework.Chore, 0), nil
+	}
+
+	df, err := os.Open(dataFile)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := df.Close(); err != nil {
+			fmt.Printf("closing data file: %v", err)
+		}
+	}()
+
+	return storage.Load(df)
+}
+
+func flush(chores []*housework.Chore) error {
+	df, err := os.Create(dataFile)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := df.Close(); err != nil {
+			fmt.Printf("closing data file: %v", err)
+		}
+	}()
+
+	return storage.Flush(df, chores)
+}
+
+func list() error {
+	chores, err := load()
+	if err != nil {
+		return err
+	}
+
+	if len(chores) == 0 {
+		fmt.Println("You're all caught up")
+	}
+
+	fmt.Println("#\t[X]\tDescription")
+	for i, chore := range chores {
+		c := ""
+		if chore.Complete {
+			c = "X"
+		}
+		fmt.Println("%d\t[%s]\t%s\n", i+1, c, chore.Description)
+	}
+
+	return nil
+}
+
+func add(s string) error {
+	chores, err := load()
+	if err != nil {
+		return err
+	}
+
+	for _, chore := range strings.Split(s, ", ") {
+		if desc := strings.TrimSpace(chore); desc != "" {
+			chores = append(chores, &housework.Chore{
+				Description: desc,
+			})
+		}
+	}
+
+	return flush(chores)
+}
+
+func complete(s string) error {
+	idx, err := strconv.Atoi(s)
+	if err != nil {
+		return err
+	}
+
+	chores, err := load()
+	if err != nil {
+		return err
+	}
+
+	if idx < 1 || idx > len(chores) {
+		return fmt.Errorf("chore %d not found", idx)
+	}
+
+	chores[idx-1].Complete = true
+
+	return flush(chores)
+}
---
+++ network/ch12/go.mod
@@ -0,0 +1,3 @@
+module github.com/ZeroBl21/network/ch12
+
+go 1.23.1
