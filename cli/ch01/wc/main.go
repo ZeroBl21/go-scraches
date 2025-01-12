7676fd4bbb2dc09737c02fef93096003f4ce7743
Darwin Perdomo <ZeroBl@hotmail.com>
2025-01-12 17:01:27 -0400

    feat(cli/ch01/wc): Add count bytes flag

--- cli/ch01/wc/main.go
+++ cli/ch01/wc/main.go
@@ -8,18 +8,36 @@ import (
 	"os"
 )
 
+const (
+	CountLines = 1 << iota
+	CountBytes
+)
+
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Bytes lines")
 
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	flags := 0
+	if *lines {
+		flags |= CountLines
+	}
+	if *bytes {
+		flags |= CountBytes
+	}
+
+	fmt.Println(count(os.Stdin, flags))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, flags int) int {
 	scanner := bufio.NewScanner(r)
 
-	if !countLines {
+	if flags&CountLines != 0 {
+		scanner.Split(bufio.ScanLines)
+	} else if flags&CountBytes != 0 {
+		scanner.Split(bufio.ScanBytes)
+	} else {
 		scanner.Split(bufio.ScanWords)
 	}
 
--- cli/ch01/wc/main_test.go
+++ cli/ch01/wc/main_test.go
@@ -9,7 +9,7 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 
 	exp := 4
-	res := count(b, false)
+	res := count(b, 0)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
@@ -20,7 +20,18 @@ func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
 
 	exp := 3
-	res := count(b, true)
+	res := count(b, CountLines)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+
+	exp := 35
+	res := count(b, CountBytes)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
