f3a68fc30c63d7233ebacd4e41ce413d4bfc6def
Darwin Perdomo <ZeroBl@hotmail.com>
2025-01-13 18:30:11 -0400

    feat(cli/ch02/todo): Implemented 'complete', 'list' and 'add' with flags

--- cli/ch02/todo/cmd/todo/main.go
+++ cli/ch02/todo/cmd/todo/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
-	"strings"
 
 	todo "github.com/ZeroBl21/cli/ch02"
 )
@@ -12,6 +11,12 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	task := flag.String("task", "", "Task to be included in the To Do list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completedList all tasks")
+
+	flag.Parse()
+
 	l := &todo.List{}
 
 	if err := l.Get(todoFileName); err != nil {
@@ -19,17 +24,30 @@ func main() {
 	}
 
 	switch {
-	case len(os.Args) == 1:
+	case *list:
 		for _, item := range *l {
-			fmt.Println(item.Task)
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
 		}
-	default:
-		item := strings.Join(os.Args[1:], " ")
 
-		l.Add(item)
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			log.Fatal(err)
+		}
 
 		if err := l.Save(todoFileName); err != nil {
 			log.Fatal(err)
 		}
+
+	case *task != "":
+		l.Add(*task)
+
+		if err := l.Save(todoFileName); err != nil {
+			log.Fatal(err)
+		}
+
+	default:
+		flag.PrintDefaults()
 	}
 }
