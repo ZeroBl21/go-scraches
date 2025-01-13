3a3575bf95df34f4b40b0e6074bd4797c97344c8
Darwin Perdomo <ZeroBl@hotmail.com>
2025-01-13 17:33:58 -0400

    feat(cli/ch02/todo): Add the list and append implementations with test to main package

--- cli/ch02/todo/cmd/todo/main.go
+++ cli/ch02/todo/cmd/todo/main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	todo "github.com/ZeroBl21/cli/ch02"
+)
+
+const todoFileName = ".todo.json"
+
 func main() {
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		log.Fatal(err)
+	}
+
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	default:
+		item := strings.Join(os.Args[1:], " ")
+
+		l.Add(item)
+
+		if err := l.Save(todoFileName); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
