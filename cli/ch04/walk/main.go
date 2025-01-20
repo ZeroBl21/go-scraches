e996e949ccf2ae096e347fc2ce1ce59a8a0874ce
Darwin Perdomo <ZeroBl@hotmail.com>
2025-01-20 16:54:17 -0400

    feat(cli/ch04/walk): Add delete flag for delete the matching files

--- cli/ch04/walk/main.go
+++ cli/ch04/walk/main.go
@@ -10,16 +10,19 @@ import (
 )
 
 type config struct {
-	ext  string
-	size int64
-	list bool
+	ext    string
+	size   int64
+	list   bool
+	delete bool
 }
 
 func main() {
 	c := config{}
 
 	root := flag.String("root", ".", "Root directory to start")
+
 	flag.BoolVar(&c.list, "list", false, "List files only")
+	flag.BoolVar(&c.delete, "del", false, "Delete files")
 	flag.StringVar(&c.ext, "ext", "", "File extension to filter out")
 	flag.Int64Var(&c.size, "size", 0, "Minimum file size")
 
@@ -42,6 +45,10 @@ func run(root string, out io.Writer, cfg config) error {
 
 		if cfg.list {
 			return listFile(path, out)
+		}
+
+		if cfg.delete {
+			return deleteFile(path)
 		}
 
 		return listFile(path, out)
