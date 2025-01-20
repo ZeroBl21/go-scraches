dee123c26f21a0d2167a590a39de4b24861ceabb
Darwin Perdomo <ZeroBl@hotmail.com>
2025-01-20 16:26:01 -0400

    feat(cli/ch04/walk): Implementing base walk function

---
+++ cli/ch04/walk/go.mod
@@ -0,0 +1,3 @@
+module github.com/ZeroBl21/cli/ch04/walk
+
+go 1.23.1
---
+++ cli/ch04/walk/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type config struct {
+	ext  string
+	size int64
+	list bool
+}
+
+func main() {
+	c := config{}
+
+	root := flag.String("root", ".", "Root directory to start")
+	flag.BoolVar(&c.list, "list", false, "List files only")
+	flag.StringVar(&c.ext, "ext", "", "File extension to filter out")
+	flag.Int64Var(&c.size, "size", 0, "Minimum file size")
+
+	flag.Parse()
+
+	if err := run(*root, os.Stdout, c); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(root string, out io.Writer, cfg config) error {
+	return filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if filterOut(path, cfg.ext, cfg.size, info) {
+			return nil
+		}
+
+		if cfg.list {
+			return listFile(path, out)
+		}
+
+		return listFile(path, out)
+	})
+}
