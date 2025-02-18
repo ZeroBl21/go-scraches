b397a065d2e5ee201133d42b178c7a95023a416b
Darwin Perdomo <ZeroBl@hotmail.com>
2025-02-18 13:52:06 -0400

    feat(cli/ch09/pomo): Add widgets business logic

---
+++ cli/ch09/pomo/pomodoro/app/buttons.go
@@ -0,0 +1,93 @@
+package app
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ZeroBl21/cli/ch09/pomo/pomodoro"
+	"github.com/mum4k/termdash/cell"
+	"github.com/mum4k/termdash/widgets/button"
+)
+
+type buttonSet struct {
+	btnStart *button.Button
+	btnPause *button.Button
+}
+
+func newButtonSet(
+	ctx context.Context,
+	config *pomodoro.IntervalConfig,
+	w *widgets,
+	redrawCh chan<- bool,
+	errCh chan<- error,
+) (*buttonSet, error) {
+	startInterval := func() {
+		i, err := pomodoro.GetInterval(config)
+		errCh <- err
+
+		start := func(pomodoro.Interval) {
+			msg := "Take a break"
+			if i.Category == pomodoro.CategoryPomodoro {
+				msg = "Focus on your task"
+			}
+
+			w.update([]int{}, i.Category, msg, "", redrawCh)
+		}
+
+		periodic := func(pomodoro.Interval) {
+			w.update(
+				[]int{int(i.ActualDuration), int(i.PlannedDuration)},
+				"", "", fmt.Sprint(i.PlannedDuration-i.ActualDuration), redrawCh)
+		}
+
+		end := func(pomodoro.Interval) {
+			w.update([]int{}, "", "Nothing running...", "", redrawCh)
+		}
+
+		errCh <- i.Start(ctx, config, start, periodic, end)
+	}
+
+	pauseInterval := func() {
+		i, err := pomodoro.GetInterval(config)
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		if err := i.Pause(config); err != nil {
+			if err == pomodoro.ErrIntervalNotRunning {
+				return
+			}
+			errCh <- err
+			return
+		}
+
+		w.update([]int{}, "", "Paused... press start to continue", "", redrawCh)
+	}
+
+	btnStart, err := button.New("(S)tart", func() error {
+		go startInterval()
+		return nil
+	},
+		button.GlobalKey('s'),
+		button.WidthFor("(P)ause"),
+		button.Height(2),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	btnPause, err := button.New("(S)tart", func() error {
+		go pauseInterval()
+		return nil
+	},
+		button.FillColor(cell.ColorNumber(220)),
+		button.GlobalKey('p'),
+		button.Height(2),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &buttonSet{btnStart, btnPause}, nil
+}
