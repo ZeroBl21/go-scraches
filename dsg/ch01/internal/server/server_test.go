ceedd0838604f5f56798dc66955f422fcef5f03a
Darwin Perdomo <ZeroBl@hotmail.com>
2025-04-01 14:53:09 -0400

    feat(server): Add zap implementation and opencensus logging

--- dsg/ch01/internal/server/server_test.go
+++ dsg/ch01/internal/server/server_test.go
@@ -2,19 +2,39 @@ package server
 
 import (
 	"context"
+	"flag"
 	"net"
+	"os"
 	"testing"
+	"time"
 
 	api "github.com/ZeroBl21/dsg/ch01/proglog/api/v1"
 	"github.com/ZeroBl21/dsg/ch01/proglog/internal/auth"
 	"github.com/ZeroBl21/dsg/ch01/proglog/internal/config"
 	"github.com/ZeroBl21/dsg/ch01/proglog/internal/log"
 	"github.com/stretchr/testify/require"
+	"go.opencensus.io/examples/exporter"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 )
+
+var debug = flag.Bool("debug", false, "Enable observability for debugging.")
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	if *debug {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			panic(err)
+		}
+		zap.ReplaceGlobals(logger)
+	}
+
+	os.Exit(m.Run())
+}
 
 func TestServer(t *testing.T) {
 	for scenario, fn := range map[string]func(
@@ -97,6 +117,28 @@ func setupTest(t *testing.T, fn func(*Config)) (
 	require.NoError(t, err)
 
 	authorizer := auth.New(config.ACLModelFile, config.ACLPolicyFile)
+
+	var telemetryExporter *exporter.LogExporter
+	if *debug {
+		metricsLogFile, err := os.CreateTemp("", "metrics-*.log")
+		require.NoError(t, err)
+		t.Logf("metrcis log file: %s", metricsLogFile.Name())
+
+		tracesLogFile, err := os.CreateTemp("", "traces-*.log")
+		require.NoError(t, err)
+		t.Logf("metrcis log file: %s", tracesLogFile.Name())
+
+		telemetryExporter, err = exporter.NewLogExporter(
+			exporter.Options{
+				MetricsLogFile:    metricsLogFile.Name(),
+				TracesLogFile:     tracesLogFile.Name(),
+				ReportingInterval: time.Second,
+			})
+		require.NoError(t, err)
+		err = telemetryExporter.Start()
+		require.NoError(t, err)
+	}
+
 	cfg = &Config{
 		CommitLog:  cLog,
 		Authorizer: authorizer,
@@ -117,6 +159,12 @@ func setupTest(t *testing.T, fn func(*Config)) (
 		rootConn.Close()
 		nobodyConn.Close()
 		l.Close()
+
+		if telemetryExporter != nil {
+			time.Sleep(1500 * time.Millisecond)
+			telemetryExporter.Stop()
+			telemetryExporter.Close()
+		}
 	}
 }
 
