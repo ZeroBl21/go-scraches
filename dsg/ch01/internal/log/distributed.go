6ad0ff7d56a35e06868b0419507cbff28f35c9a8
Darwin Perdomo <ZeroBl@hotmail.com>
2025-04-08 13:39:27 -0400

    feat(log/distributed): Add log API and Raft to the service

---
+++ dsg/ch01/internal/log/distributed.go
@@ -0,0 +1,342 @@
+package log
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+
+	api "github.com/ZeroBl21/dsg/ch01/proglog/api/v1"
+	"github.com/hashicorp/raft"
+	raftboltdb "github.com/hashicorp/raft-boltdb"
+	"google.golang.org/protobuf/proto"
+)
+
+type DistributedLog struct {
+	config Config
+	log    *Log
+	raft   *raft.Raft
+}
+
+func NewDistributedLog(dataDir string, config Config) (*DistributedLog, error) {
+	l := &DistributedLog{
+		config: config,
+	}
+
+	if err := l.setupLog(dataDir); err != nil {
+		return nil, err
+	}
+
+	if err := l.setupRaft(dataDir); err != nil {
+		return nil, err
+	}
+
+	return l, nil
+}
+
+func (d *DistributedLog) setupLog(dataDir string) error {
+	logDir := filepath.Join(dataDir, "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+
+	var err error
+	d.log, err = NewLog(logDir, d.config)
+
+	return err
+}
+
+func (d *DistributedLog) setupRaft(dataDir string) error {
+	fsm := &fsm{log: d.log}
+
+	logDir := filepath.Join(dataDir, "raft", "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+
+	logConfig := d.config
+	logConfig.Segment.InitialOffset = 1
+	logStore, err := newLogStore(logDir, logConfig)
+	if err != nil {
+		return err
+	}
+
+	stableStore, err := raftboltdb.NewBoltStore(
+		filepath.Join(dataDir, "raft", "stable"),
+	)
+	if err != nil {
+		return err
+	}
+
+	retain := 1
+	snapshotStore, err := raft.NewFileSnapshotStore(
+		filepath.Join(dataDir, "raft"),
+		retain,
+		os.Stderr,
+	)
+	if err != nil {
+		return err
+	}
+
+	maxPool := 5
+	timeout := 10 * time.Second
+	transport := raft.NewNetworkTransport(
+		d.config.Raft.StreamLayer,
+		maxPool,
+		timeout,
+		os.Stderr,
+	)
+
+	config := raft.DefaultConfig()
+	config.LocalID = d.config.Raft.LocalID
+
+	if d.config.Raft.HeartbeatTimeout != 0 {
+		config.HeartbeatTimeout = d.config.Raft.HeartbeatTimeout
+	}
+
+	if d.config.Raft.ElectionTimeout != 0 {
+		config.ElectionTimeout = d.config.Raft.ElectionTimeout
+	}
+
+	if d.config.Raft.LeaderLeaseTimeout != 0 {
+		config.LeaderLeaseTimeout = d.config.Raft.LeaderLeaseTimeout
+	}
+
+	if d.config.Raft.CommitTimeout != 0 {
+		config.CommitTimeout = d.config.Raft.CommitTimeout
+	}
+
+	d.raft, err = raft.NewRaft(
+		config,
+		fsm,
+		logStore,
+		stableStore,
+		snapshotStore,
+		transport,
+	)
+	if err != nil {
+		return err
+	}
+
+	hasState, err := raft.HasExistingState(logStore, stableStore, snapshotStore)
+	if err != nil {
+		return err
+	}
+
+	if d.config.Raft.Bootstrap && !hasState {
+		config := raft.Configuration{
+			Servers: []raft.Server{{
+				ID:      config.LocalID,
+				Address: transport.LocalAddr(),
+			}},
+		}
+		err = d.raft.BootstrapCluster(config).Error()
+	}
+
+	return err
+}
+
+func (d *DistributedLog) Append(record *api.Record) (uint64, error) {
+	res, err := d.apply(AppendRequestType, &api.ProduceRequest{Record: record})
+	if err != nil {
+		return 0, err
+	}
+
+	return res.(*api.ProduceResponse).Offset, nil
+}
+
+func (d *DistributedLog) apply(
+	reqType RequestType,
+	req proto.Message,
+) (any, error) {
+	var buf bytes.Buffer
+
+	if _, err := buf.Write([]byte{byte(reqType)}); err != nil {
+		return nil, err
+	}
+
+	b, err := proto.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = buf.Write(b); err != nil {
+		return nil, err
+	}
+
+	timeout := 10 * time.Second
+	future := d.raft.Apply(buf.Bytes(), timeout)
+	if future.Error() != nil {
+		return nil, future.Error()
+	}
+
+	res := future.Response()
+	if err, ok := res.(error); ok {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (d *DistributedLog) Read(offset uint64) (*api.Record, error) {
+	return d.log.Read(offset)
+}
+
+var _ raft.FSM = (*fsm)(nil)
+
+// Finite State Machine
+type fsm struct {
+	log *Log
+}
+
+type RequestType uint8
+
+const AppendRequestType RequestType = 0
+
+func (f *fsm) Apply(record *raft.Log) any {
+	buf := record.Data
+	reqType := RequestType(buf[0])
+
+	switch reqType {
+	case AppendRequestType:
+		return f.applyAppend(buf[1:])
+	}
+
+	return nil
+}
+
+func (f *fsm) applyAppend(b []byte) any {
+	var req api.ProduceRequest
+	if err := proto.Unmarshal(b, &req); err != nil {
+		return err
+	}
+
+	offset, err := f.log.Append(req.Record)
+	if err != nil {
+		return nil
+	}
+
+	return &api.ProduceResponse{Offset: offset}
+}
+
+func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
+	return &snapshot{
+		reader: f.log.Reader(),
+	}, nil
+}
+
+func (f *fsm) Restore(r io.ReadCloser) error {
+	b := make([]byte, lenWidth)
+
+	var buf bytes.Buffer
+	for i := 0; ; i++ {
+		_, err := io.ReadFull(r, b)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		size := int64(enc.Uint64(b))
+		if _, err := io.CopyN(&buf, r, size); err != nil {
+			return err
+		}
+
+		record := &api.Record{}
+		if err = proto.Unmarshal(buf.Bytes(), record); err != nil {
+			return err
+		}
+
+		if i == 0 {
+			f.log.Config.Segment.InitialOffset = record.Offset
+			if err := f.log.Reset(); err != nil {
+				return err
+			}
+		}
+
+		if _, err := f.log.Append(record); err != nil {
+			return err
+		}
+
+		buf.Reset()
+	}
+
+	return nil
+}
+
+type snapshot struct {
+	reader io.Reader
+}
+
+func (s *snapshot) Persist(sink raft.SnapshotSink) error {
+	if _, err := io.Copy(sink, s.reader); err != nil {
+		_ = sink.Cancel()
+		return err
+	}
+
+	return sink.Close()
+}
+
+func (s *snapshot) Release() {}
+
+var _ raft.LogStore = (*logStore)(nil)
+
+type logStore struct {
+	*Log
+}
+
+func newLogStore(dir string, c Config) (*logStore, error) {
+	log, err := NewLog(dir, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &logStore{Log: log}, nil
+}
+
+func (l *logStore) FirstIndex() (uint64, error) {
+	return l.LowestOffset()
+}
+
+func (l *logStore) LastIndex() (uint64, error) {
+	return l.HighestOffset()
+}
+
+func (l *logStore) GetLog(index uint64, out *raft.Log) error {
+	in, err := l.Read(index)
+	if err != nil {
+		return err
+	}
+
+	out.Data = in.Value
+	out.Index = in.Offset
+	out.Type = raft.LogType(in.Type)
+	out.Term = in.Term
+
+	return nil
+}
+
+func (l *logStore) StoreLog(record *raft.Log) error {
+	return l.StoreLogs([]*raft.Log{record})
+}
+
+func (l *logStore) StoreLogs(records []*raft.Log) error {
+	for _, record := range records {
+		_, err := l.Append(&api.Record{
+			Value: record.Data,
+			Term:  record.Term,
+			Type:  uint32(record.Type),
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (l *logStore) DeleteRange(_, max uint64) error {
+	return l.Truncate(max)
+}
